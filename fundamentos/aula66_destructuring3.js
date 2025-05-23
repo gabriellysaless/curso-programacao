64f08913d7aa89854932e386c754c78f804ba19f
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-05-23 13:54:13 -0300

    Comentário acrescentado

--- fundamentos/aula66_destructuring3.js
+++ fundamentos/aula66_destructuring3.js
@@ -7,4 +7,4 @@ const obj = {max: 50, min: 40}
 console.log(rand(obj))
 console.log(rand({min: 955 }))
 console.log(rand({}))
-// console.log(rand()) => neste carro dará erro pq não foi passado nenhum objeto
+// console.log(rand()) => neste carro dará erro pq não foi passado nenhum objeto, não tem como desestruturar um valor nulo ou indefinido
