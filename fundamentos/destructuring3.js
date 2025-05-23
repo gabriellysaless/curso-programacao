5a32684d1039ae44915af6d5b7fca9fa9c814450
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-05-23 10:24:44 -0300

    Destructuring com função

---
+++ fundamentos/destructuring3.js
@@ -0,0 +1,10 @@
+function rand({ min = 0, max = 1000}) { //{ min = 0, max = 1000} => operador destructuring
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor) //retorna o número inteiro
+}
+
+const obj = {max: 50, min: 40}
+console.log(rand(obj))
+console.log(rand({min: 955 }))
+console.log(rand({}))
+// console.log(rand()) => neste carro dará erro pq não foi passado nenhum objeto
