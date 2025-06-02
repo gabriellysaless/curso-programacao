418acfb7f6f6c2d1d1405e4a54f63346e6df10d0
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-06-02 15:40:22 -0300

    Aula simples sobre if/else

---
+++ controle/aula79_ifElse.js
@@ -0,0 +1,10 @@
+function imprimirResultado(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(8.5)
+imprimirResultado(5.0)
